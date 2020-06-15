0d5d33bf43afc3bab040c70870422cc5a8320fae
Nick Mwangemi <nickmwangemi@gmail.com>
2020-06-15 08:44:57 +0300

    Updates

---
+++ precache-manifest.b7c393f243f61ae8e76a24ffe02c0d3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa0fcf2d39a78fe74bc7544219865a8",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "1aa1cfc40f2362bca12d",
+    "url": "/React-Todo/static/css/main.f86d78da.chunk.css"
+  },
+  {
+    "revision": "a2b010f33ebbd1049540",
+    "url": "/React-Todo/static/js/2.5d932b9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todo/static/js/2.5d932b9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa1cfc40f2362bca12d",
+    "url": "/React-Todo/static/js/main.1c9ac174.chunk.js"
+  },
+  {
+    "revision": "d1e2198655c93f5f3811",
+    "url": "/React-Todo/static/js/runtime-main.14fb7ae7.js"
+  }
+]);
